254e6b5aed7e403c46d9fcce8acb555ed6889797
John Lomas <jlomas@ceaaces.gob.ec>
2022-04-29 16:23:08 -0500

    Configuraciones y fetch con graphql

---
+++ src/components/ChatsListScreen/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ChatsList } from './ChatsList'
+import { ChatsNavbar } from './ChatsNavbar'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+export const ChatsListScreen = () => {
+    return (
+        <Container>
+            <ChatsNavbar />
+            <ChatsList />
+        </Container>
+    )
+}
